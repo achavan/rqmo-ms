830202139290f96f87c2cda1067d8939c8d0bcc7
Katia Santos <katia.lage@gmail.com>
2024-11-08 13:38:42 -0700

    End of scene pop up and image resources

---
+++ config.js
@@ -0,0 +1,30 @@
+import TitleScene from './scenes/TitleScene.js'
+import Specialist from './scenes/Specialist.js';
+import DoctorOfficeScene from './scenes/DoctorOfficeScene.js';
+import Phaser from 'phaser';
+import EndPhasePopUp from './scenes/EndPhasePopUp.js';
+
+const config = {
+    type: Phaser.AUTO, // Phaser will use WebGL if available, otherwise Canvas
+    width: 1500,
+    height: 800,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH, // Center the game in the window
+    },
+    dom: {
+        createContainer: true
+    },
+    autoRound: false, // No rounding for physics
+    parent: 'game-container', // The DOM element to contain the game
+    physics: {
+        default: 'arcade', // Use the arcade physics engine
+        arcade: {
+            gravity: { y: 0 },
+            debug: true, // Set to true to enable physics debugging
+        },
+    },
+    scene: [TitleScene, DoctorOfficeScene, EndPhasePopUp, Specialist], 
+};
+
+export default config;
